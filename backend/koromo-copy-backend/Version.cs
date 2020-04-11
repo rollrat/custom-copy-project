46bc419361df25bde198a43650c595f774d72220
rollrat <rollrat.cse@gmail.com>
2020-04-11 14:17:46 +0900

    backend: Add os specific build batch files

--- backend/koromo-copy-backend/Version.cs
+++ backend/koromo-copy-backend/Version.cs
@@ -11,7 +11,7 @@ namespace koromo_copy_backend
     {
         public const int MajorVersion = 2020;
         public const int MinorVersion = 04;
-        public const int BuildVersion = 08;
+        public const int BuildVersion = 11;
 
         public const string Name = "Koromo Copy Project";
         public static string Text { get; } = $"{MajorVersion}.{MinorVersion}.{BuildVersion}";
